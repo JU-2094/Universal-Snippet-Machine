21c7335bfbde397e798e4d4b101cc784f2cf28eb
JU-2094 <urbgon@gmail.com>
2017-04-29 23:43:31 -0500

    Crawling from google to get snippets and following next pages

--- googleapi/googleapi/spiders/GoogleSearch.py
+++ googleapi/googleapi/spiders/GoogleSearch.py
@@ -3,64 +3,47 @@
 """
     Requirements:
         pip install --upgrade google-api-python-client
-        
-        Google API key.  AIzaSyD5BYgxBrUFtDAlnAYvhpNhEFxrtdF2LHc 
     Spider to extract the urls and data from a request to google
     usage:
-        scrapy crawl googlespider -a search='Jose Martínez'     
-        
+        scrapy crawl googlespider -a search='path_to_queries_file' 
+    
+    Notes. In PyCharm, the directories must be marked as sources root
 """
 #Todo: RUTA DE ARCHIVO con querys
 
 import scrapy
 from scrapy.selector import Selector
-from scrapy.http import Request
-from googleapiclient.discovery import build
-from googleapi.googleapi.GoogleClient import GoogleClient
+from scrapy.http import Request,FormRequest
+from googleapi.GoogleClient import GoogleClient
 
 class GoogleSpider(scrapy.Spider):
     name = "googlespider"
+    base_url = "https://www.google.com/"
     start_urls = ["https://www.google.com/"]
 
     def __init__(self,search=None,*args,**kwargs):
         super(GoogleSpider,self).__init__(*args,**kwargs)
+
+        ##Todo Adapt this to the path
         if search is not None:
             self.search = search.replace(" ","+")
         else:
             self.search = ""
 
-    def generate_url(self):
-        # url_search = self.base_url + "#q="+self.search
-
-        pass
-
-    #def start_requests(self):
-        #yield scrapy.Request(self.base_url,method='POST',self.parse,cookies={"name":"q","value":self.search})
-        #yield scrapy.Request(self.generate_url(), self.parse)
-        #pass
-
-
+    def get_query(self):
+        # Todo from file get the search queries
+        yield 'Jorge+Flores'
 
     def parse(self, response):
-        #self.log("--------------------------------")
-        #urls = response.xpath("//div[@class='rc']").extract()
-        #self.log(urls)
+        for query in self.get_query():
+            yield FormRequest.from_response(response,formdata={'q':query},callback=self.collect_snippet)
 
-        return scrapy.FormRequest.from_response(response,formdata={'q':'Jorge+Flores'},callback=self.method)
+    def collect_snippet(self,response):
+        snippets = response.xpath("//div[@class='g']").extract()
 
-        #self.log(response.xpath("//body").extract())
+        for snippet in snippets:
+           #Todo Create json or fill DB
+           pass
 
-    def method(self,response):
-        self.log("-----------Mirame!!!!!----------")
-        #self.log(response.xpath("//body").extract())
-
-        urls = response.xpath("//body").extract()
-        self.log(urls)
-
-        for info in urls:
-            yield {"data":info}
-
-
-
-
-
+        for url in response.xpath("//td[@class='b']/a/@href").extract():
+            yield Request(self.base_url+url, callback=self.collect_snippet)
