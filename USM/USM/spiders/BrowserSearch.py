0cbeb7e083bafc6426d8f56c9d59fd7284331c71
JU-2094 <urbgon@gmail.com>
2017-05-04 12:46:58 -0500

    Spider to request and crawl from browsers

--- USM/USM/spiders/BrowserSearch.py
+++ USM/USM/spiders/BrowserSearch.py
@@ -6,58 +6,58 @@
     Spider to extract the urls and data from a request to google
     usage:
         scrapy crawl googlespider -a file='path_to_queries_file' 
-    
+
     Notes. In PyCharm, the directories must be marked as sources root
-    
-    URBS -> Universal Research Basic Search
+
+    USM -> Universal Snippet Machine
 """
-#Todo: Storage items | find a way to run other threads for googleclient api
-#Todo: search module->subprocess
-#Todo: Use more browsers, the scientist and yahoo,bing,duckduckgo   ETC.
+# Todo: Storage items | find a way to run other threads for googleclient api
+# Todo: search module->subprocess
+# Todo: Use more browsers, the scientist and yahoo,bing,duckduckgo   ETC.
 
 import scrapy
 from scrapy import Selector
-from scrapy.http import Request,FormRequest
+from scrapy.http import Request, FormRequest
 from googleapi.GoogleClient import GoogleClient
 
-class GoogleSpider(scrapy.Spider):
-    name = "googlespider"
+
+class BrowserSearch(scrapy.Spider):
+    name = "browserspider"
     base_url = "https://www.google.com/"
     start_urls = ["https://www.google.com/"]
 
-    def __init__(self,file=None,*args,**kwargs):
-        super(GoogleSpider,self).__init__(*args,**kwargs)
+    def __init__(self, file=None, *args, **kwargs):
+        super(BrowserSearch, self).__init__(*args, **kwargs)
         if file is not None:
             self.file = file
         else:
             self.file = ""
 
     def get_query(self):
-        with open(self.file,'r') as f:
+        with open(self.file, 'r') as f:
             for line in f:
                 yield line
 
     def parse(self, response):
         # for query in self.get_query():
-        #     yield FormRequest.from_response(response, 
+        #     yield FormRequest.from_response(response,
         #                                     formdata={'q':query},
         #                                     callback=self.collect_snippet)
         yield FormRequest.from_response(response,
                                         formdata={'q': '"Jorge Flores Valdéz"'},
                                         callback=self.collect_snippet)
 
-    def collect_snippet(self,response):
+    def collect_snippet(self, response):
         snippets = response.xpath("//div[@class='g']").extract()
 
-        self._create_page(response.body,"-test")
+        self._create_page(response.body, "-test")
 
         for snippet in snippets:
-           #Todo Create json or fill DB
-           self.log("----------------")
-           self.log(snippet)
+            # Todo Create json or fill DB
+            self.log("----------------")
+            self.log(snippet)
 
-
-           pass
+            pass
 
         number = response.xpath("//td/b/text()").extract()
 
@@ -65,8 +65,7 @@ class GoogleSpider(scrapy.Spider):
         #     for url in response.xpath("//td[@class='b']/a/@href").extract():
         #          yield Request(self.base_url+url, callback=self.collect_snippet)
 
-
-    def _create_page(self,body,name):
-        file ="web%s.html" % name
-        with open(file,'wb') as f:
+    def _create_page(self, body, name):
+        file = "web%s.html" % name
+        with open(file, 'wb') as f:
             f.write(body)
--- googleapi/googleapi/GoogleClient.py
+++ googleapi/googleapi/GoogleClient.py
@@ -1,25 +1,31 @@
 import pprint
 import logging as log
+import threading
 from googleapiclient.discovery import build
+#ToDo storage the json recovered
 
-
-class GoogleClient:
+class GoogleClient(threading.Thread):
     key = "AIzaSyD5BYgxBrUFtDAlnAYvhpNhEFxrtdF2LHc"
     customEngine = '017834202398449870076:eeqbhmbsv90'
+    max_queries = 100   #The API allows 100 per day
 
-    def __init__(self):
-        self.search = ''
+    def __init__(self,search):
+        threading.Thread.__init__(self)
+        self.search = search
         self.service = build("customsearch", "v1",
                              developerKey=self.key)
 
+    def run(self):
+        log.INFO("Getting json from customsearch api")
+        self._getjson()
+        log.INFO("End of THREAD")
 
-
-    def get_snippet(self,search):
+    def _getjson(self):
         start = 1
         try:
             while True:
                 res = self.service.cse().list(
-                    q='Ivan',
+                    q=self.search,
                     cx=self.customEngine,
                     start=str(start)
                 ).execute()
@@ -27,8 +33,8 @@ class GoogleClient:
                 yield res
 
                 start = start + 10
-                if start>100:
+                if start > self.max_queries:
                     break
         except:
-            log.error(" API_GOOGLE CUSTOM_SEARCH: %s","Cuote of 100 queries per day surpased")
+            log.error(" API_GOOGLE CUSTOM_SEARCH: %s", "Cuote of 100 queries per day surpased")
             return None
